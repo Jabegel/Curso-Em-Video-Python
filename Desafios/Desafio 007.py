78ac31e120df0aa9a5a39b0c14e5d60135dffcd3
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2024-11-25 08:51:59 -0300

    Atualizar o Desafio 007.py

--- Desafios/Desafio 007.py
+++ Desafios/Desafio 007.py
@@ -1,1 +1,7 @@
-# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+# Desenvolva um programa que leia as duas notas 
+# de um aluno, calcule e mostre a sua média
+
+N1 = float(input('Digite a primeira nota: '))
+N2 = float(input('Dimite a segunda nota: '))
+
+print('A média entre {:.2f} e {:.2f} é {:.2f} .'.format(N1, N2, (N1 + N2)/2))
