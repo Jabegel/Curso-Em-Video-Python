5b83c353818780e7bbad315e0890fef6cb826b1c
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2024-11-25 08:32:58 -0300

    Atualizar o Desafio 005.py

--- Desafios/Desafio 005.py
+++ Desafios/Desafio 005.py
@@ -1,1 +1,7 @@
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+N = int(input('Digite seu número: '))
+ant = N - 1
+suc = N + 1
+
+print('O antecessor de {} é {} e o seu sucessor é {}.'.format(N, ant, suc))
