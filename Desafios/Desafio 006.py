45e93a76cf2d4205e937a25d46c6a954079bdddc
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2024-11-25 08:39:23 -0300

    Atualizar o Desafio 006.py

--- Desafios/Desafio 006.py
+++ Desafios/Desafio 006.py
@@ -1,1 +1,9 @@
-# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+# Crie um algoritmo que leia um número e 
+# mostre o seu dobro, triplo e raiz quadrada
+
+N = int(input('Digite um número: '))
+d = N * 2
+t = N * 3
+r = N ** 0.5
+
+print('O dobro de {} é {}, seu triplo {} e sua raiz quadrada é {} .'.format(N, d, t, r))
